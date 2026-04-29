bea46a36c8049f796c060d39c35e95e58d0f92b5
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-04-29 11:34:38 +0530

    basic java coding
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ day1.java
@@ -0,0 +1,11 @@
+class day1{
+    public static void main(String[] args) {
+        String s = "abc";
+        //System.out.print(s);
+        
+        String a = new String("abc");
+        System.out.println(a);
+        System.out.println(s==a);
+        System.out.println(s.equals(a));
+            }
+} 
