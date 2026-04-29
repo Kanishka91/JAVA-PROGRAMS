643cd215b34951766c8454f640886c6d91117ce8
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-04-29 11:35:29 +0530

    usage of logical operatorss using java language
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ logical.java
@@ -0,0 +1,10 @@
+public class logical {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a && b); // AND
+        System.out.println(a || b); // OR
+        System.out.println(!a);     // NOT
+    }
+}
