e06c085b31417cf68802f525c7a9398e123b6b6c
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-04-30 14:01:32 +0530

    Printing pass marks using if-else
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

--- score.java
+++ score.java
@@ -1,21 +1,9 @@
-class Score {
+class score {
     public static void main(String[] args) {
-        int score = 85;
+        int marks = 45;
 
-        if (score >= 90) {
-            System.out.println("Grade A");
-        } 
-        else if (score >= 80) {
-            System.out.println("Grade B");
-        } 
-        else if (score >= 70) {
-            System.out.println("Grade C");
-        } 
-        else if (score >= 60) {
-            System.out.println("Grade D");
-        } 
-        else if (score >= 50) {
-            System.out.println("Grade E");
+        if (marks >= 40) {
+            System.out.println("Pass");
         } 
         else {
             System.out.println("Fail");
