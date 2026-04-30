105d2872ab850dbb0f35c052c991ba3c0424781b
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-04-30 13:59:14 +0530

    printing largest number
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ large.java
@@ -0,0 +1,16 @@
+class large {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 40;
+
+        if (a > b) {
+            System.out.println(a);
+        } 
+        else if (b > a) {
+            System.out.println(b);
+        } 
+        else {
+            System.out.println("Equal");
+        }
+    }
+}
