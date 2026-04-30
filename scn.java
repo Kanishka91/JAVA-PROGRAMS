b71171169386c7e87cd9219b037dd562e268ed4d
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-04-30 14:10:38 +0530

    Printing the age using scanner class
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ scn.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class scn{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the age: ");
+        int age=s.nextInt();
+        System.out.println(age);
+
+    }
+}
