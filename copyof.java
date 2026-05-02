bd1d7bef9ec43f3d5cea593ae51b15095ad8c4c9
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-05-02 11:36:28 +0530

    using Arrays.copyOf() in arrays
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ copyof.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class copyof{
+    public static void main(String[] args) {
+        int a[] = {10,20,30,40,50};
+        int b[] = new int[5];
+
+        
+
+        b=Arrays.copyOf(a,3);
+        for(int i=0;i<b.length;i++){
+            System.out.println(b[i]);
+        }
+    }
+}
