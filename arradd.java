9da6050172707855260855573ee71f7cdbb404d0
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-05-02 11:34:46 +0530

    Exploring about array creation , remove and replacing
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ arradd.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+class arradd{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(5);
+        arr.add(6);
+        arr.add(77);
+        System.out.println(arr);
+        arr.set(2,7);
+        System.out.println(arr);
+        arr.remove(1);
+        System.out.println(arr);
+    }
+}
