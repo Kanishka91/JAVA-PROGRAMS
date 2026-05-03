dea2379aa81e84e22c2209efa495b941235fc6db
Kanishka91 <kanishkabj.23ece@kongu.edu>
2026-05-03 11:16:13 +0530

    Using collections.sort() method
    Signed-off-by: Kanishka91 <kanishkabj.23ece@kongu.edu>

---
+++ sort1.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+class sort1{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(5);
+        arr.add(7);
+        arr.add(77);
+        arr.add(19);
+        arr.add(30);
+        System.out.println(arr);
+        Collections.sort(arr);
+        System.out.println(arr);
+       
+    }
+}
